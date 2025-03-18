e63433a1bdbe4be93fa9aa3600056508e9ca32d8
FPS MUN <munfps@gmail.com>
2025-03-18 23:55:26 +0500

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/fps-mun-website/',
   server: {
     host: "::",
     port: 8080,
