4e05247a73f7542bfa375a96fa6d8ae6b8dc88db
FPS MUN <munfps@gmail.com>
2025-03-19 06:59:17 +0500

    Remove base path from Vite config for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/fps-mun-website/',
   server: {
     host: "::",
     port: 8080,
